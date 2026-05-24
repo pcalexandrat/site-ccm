665a68a720413b6ca7dfcef4ffe6511e499f99e3
Alexandra <127262337+pcalexandrat@users.noreply.github.com>
2026-05-24 19:28:05 -0300

    .

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ const MIN_FONT_SIZE = 14;
 const MAX_FONT_SIZE = 22;
 let currentFontSize = DEFAULT_FONT_SIZE;
 
-const applyFontSize = () => {
-  document.documentElement.style.fontSize = `${currentFontSize}px`;
-};
+function applyFontSize() {
+    document.documentElement.style.fontSize = `${currentFontSize}px`;
+}
 
 const increaseFont = () => {
   currentFontSize = Math.min(MAX_FONT_SIZE, currentFontSize + 2);
